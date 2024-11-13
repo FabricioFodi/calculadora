141840617526ed583ba33d2076546aeb40f33b23
Fabricio <159970835+FabricioFodi@users.noreply.github.com>
2024-11-12 21:43:15 -0300

    Update mix.cy.ts

--- cypress/e2e/calculator/mix.cy.ts
+++ cypress/e2e/calculator/mix.cy.ts
@@ -63,14 +63,78 @@ describe("Calculos Combinados - Operações Mistas", () => {
     cy.get('[data-cy="display-container"]').should("have.text", "40");
   });
 
-  //Erro
-  it("1 + 2 * 3", () => {
+  // //Erro
+  // it("1 + 2 * 3", () => {
+  //   cy.get('[data-cy="1"]').click();
+  //   cy.get('[data-cy="+"]').click();
+  //   cy.get('[data-cy="2"]').click();
+  //   cy.get('[data-cy="x"]').click();
+  //   cy.get('[data-cy="3"]').click();
+  //   cy.get('[data-cy="="]').click();
+  //   cy.get('[data-cy="display-container"]').should("have.text", "7");
+  // });
+
+  it('1 + 2', () => {
     cy.get('[data-cy="1"]').click();
     cy.get('[data-cy="+"]').click();
     cy.get('[data-cy="2"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="display-container"]').should('have.text', '3');
+  });
+
+  it('1 - 1', () => {
+    cy.get('[data-cy="1"]').click();
+    cy.get('[data-cy="-"]').click();
+    cy.get('[data-cy="1"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="display-container"]').should('have.text', '0');
+  });
+
+  it('1 * 1', () => {
+    cy.get('[data-cy="1"]').click();
     cy.get('[data-cy="x"]').click();
+    cy.get('[data-cy="1"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="display-container"]').should('have.text', '1');
+  });
+
+  it('6 / 2', () => {
+    cy.get('[data-cy="6"]').click();
+    cy.get('[data-cy="/"]').click();
+    cy.get('[data-cy="2"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="display-container"]').should('have.text', '3');
+  });
+
+  it('6 + 3 = 9 * 7', () => {
+    cy.get('[data-cy="6"]').click();
+    cy.get('[data-cy="+"]').click();
     cy.get('[data-cy="3"]').click();
     cy.get('[data-cy="="]').click();
-    cy.get('[data-cy="display-container"]').should("have.text", "7");
+    cy.get('[data-cy="display-container"]').should('have.text', '9');
+    cy.get('[data-cy="x"]').click();
+    cy.get('[data-cy="7"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="display-container"]').should('have.text', '63');
+  });
+
+  it('0.3 + 0.6', () => {
+    cy.get('[data-cy="0"]').click();
+    cy.get('[data-cy="."]').click();
+    cy.get('[data-cy="3"]').click();
+    cy.get('[data-cy="+"]').click();
+    cy.get('[data-cy="0"]').click();
+    cy.get('[data-cy="."]').click();
+    cy.get('[data-cy="6"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="display-container"]').should('have.text', '0.9');
+  });
+  
+  it('. + / =', () => {
+    cy.get('[data-cy="."]').click();
+    cy.get('[data-cy="+"]').click();
+    cy.get('[data-cy="/"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="display-container"]').should('have.text', 'Resultado Indefinido');
   });
 });
